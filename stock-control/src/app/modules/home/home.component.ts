2eea130bc392c635388706342fab63ea19fad38c
Carlos Sempé <carlos.sempe@senior.com.br>
2023-11-06 07:15:15 -0300

    Created a reactve form

--- stock-control/src/app/modules/home/home.component.ts
+++ stock-control/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   loginCard = true;
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmitLoginForm() {
+    console.log('Dados do formulário de login', this.loginForm.value);
+  }
+  onSubmitSignUpForm() {
+    console.log('Dados do formulário de cadastro', this.signupForm.value);
+  }
 }
